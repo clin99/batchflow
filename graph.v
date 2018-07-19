f8226bb7df7fbc18d1cd5b86ca7d9b01b8805348
clin99 <clin99@csl-408-12.csl.illinois.edu>
2018-07-19 14:08:09 -0500

    First update

--- graph.v
+++ graph.v
@@ -1,29 +1,27 @@
-`include a.yml`
-`include b.yml`
-`include c.yml`
+import a.yml
+import b.yml
+import c.yml
 
 
 module MyModule(
   in_a1,
   in_a2,
-  in_b1,
   out_c
 );
 
-input in_a1; [/home/clin99/file1]  // Specify input file position and name? Need to send this info to a.yml 
+input in_a1;  // Specify input file position and name? Need to send this info to a.yml 
 input in_a2;
-input in_b1;
 
-output out_c;  // This will be filled by the spec in yml
+output out_c;  
+
+wire::stream a2b;  // Each wire can be associated with a file name. This file name is used for the input for next gate
+wire::depend b2c;
+
+A inst_a(.i1(in_a1), .i2(in_a2), .o1(a2b)) takes("-lib late_library -v simple.v -param 1.234 -o simple_from_a.v");
+B inst_b(.i1(a2b),   .o1(b2c))             takes("-o simple_from_b.v");
+C inst_c(.i1(b2c),   .o1(out_c))           takes("-v simple_from_b.v -o simple_from_c.v");
 
 
-
-wire a2c;  // Each wire can be associated with a file name. This file name is used for the input for next gate
-wire b2c;
-
-A inst_a(.i1(in_a1), .i2(in_a2), .o1(a2c)) []
-B inst_b(.i1(in_b1), .o1(b2c));
-C inst_c(.i1(a2c), .i2(b2c), .o1(out_c));
 
 // What if there is no dependency/IO between A, B and C but 
 // we still want them to be executed in order
